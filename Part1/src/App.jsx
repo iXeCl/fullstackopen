18c7b26ed5dc87bf714bdd4be680c1883eb30d55
iXecl <dharmick95@gmail.com>
2024-09-26 16:22:40 +1000

    Finished 1.5

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -20,9 +20,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div> 
-        <Part parts= {props.parts[0].name} exercise={props.parts[0].exercises}/>
-        <Part parts= {props.parts[1].name} exercise={props.parts[1].exercises}/>
-        <Part parts= {props.parts[2].name} exercise={props.parts[2].exercises}/>
+        <Part part= {props.parts[0].name} exercises={props.parts[0].exercises}/>
+        <Part part= {props.parts[1].name} exercises={props.parts[1].exercises}/>
+        <Part part= {props.parts[2].name} exercises={props.parts[2].exercises}/>
     </div>
 
   )
@@ -40,28 +40,29 @@ const Total = (props) =>{
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  console.log(parts)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  console.log(course)
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}> </Content>
-      <Total parts={parts}> </Total>
-      
+      <Header course={course.name}/>
+      <Content parts={course.parts}/> 
+      <Total parts={course.parts}/>      
     </div>
     
   )
--- learnjs.js
+++ learnjs.js
@@ -1,94 +1,115 @@
 
-// Variables
-const x = 1
-let y = 5 
+// // Variables
+// const x = 1
+// let y = 5 
 
-console.log(x,y)
+// console.log(x,y)
 
-y+=10
-console.log(x,y)
-y='sometext'
-console.log(x,y)
-// const sets an immutable variable whereas let declares a mutable variable
+// y+=10
+// console.log(x,y)
+// y='sometext'
+// console.log(x,y)
+// // const sets an immutable variable whereas let declares a mutable variable
 
-// Arrays
-const t= [1,-1,3]
-console.log(t)
-t.push(5)
+// // Arrays
+// const t= [1,-1,3]
+// console.log(t)
+// t.push(5)
 
-console.log(t)
-console.log(t.length)
-console.log(t[1])
+// console.log(t)
+// console.log(t.length)
+// console.log(t[1])
 
-t.forEach(value => 
-    console.log(value)
+// t.forEach(value => 
+//     console.log(value)
 
-)
+// )
 
-const t1=[1,-1,3]
-const t2=t1.concat(4)
-console.log(t1)
-console.log(t2)
-//maps
-const m1 = t.map(value=>value*2)
-console.log(t)
-console.log(m1)
-const m2  = t.map(value => '<li>' + value +'<li>')
-console.log(m2)
-//destructuring
-const t3  = [1,2,3,4,5]
-const [first,second,...rest] = t3
-console.log(first,second)
-console.log(rest)
+// const t1=[1,-1,3]
+// const t2=t1.concat(4)
+// console.log(t1)
+// console.log(t2)
+// //maps
+// const m1 = t.map(value=>value*2)
+// console.log(t)
+// console.log(m1)
+// const m2  = t.map(value => '<li>' + value +'<li>')
+// console.log(m2)
+// //destructuring
+// const t3  = [1,2,3,4,5]
+// const [first,second,...rest] = t3
+// console.log(first,second)
+// console.log(rest)
 
-//Objects
+// //Objects
 
-const object1 = {
-    name: 'Arto Hellas',
-    age: 35,
-    education: 'PhD',
-}
+// const object1 = {
+//     name: 'Arto Hellas',
+//     age: 35,
+//     education: 'PhD',
+// }
 
-const object2 = {
-    name: 'Full Stack web application development',
-    level: 'intermediate studies',
-    size: 5
-}
+// const object2 = {
+//     name: 'Full Stack web application development',
+//     level: 'intermediate studies',
+//     size: 5
+// }
 
-const object3 = {
-    name: {
-        fist: 'Dan',
-        last: 'Abramov',
-    },
-    grades: [2,3,5,3],
-    department: 'Stanford University',
-}
+// const object3 = {
+//     name: {
+//         fist: 'Dan',
+//         last: 'Abramov',
+//     },
+//     grades: [2,3,5,3],
+//     department: 'Stanford University',
+// }
 
-console.log(object1.name)
-const fieldName='age'
-console.log(object1[fieldName])
+// console.log(object1.name)
+// const fieldName='age'
+// console.log(object1[fieldName])
 
-object1.address = ['Helsinki']
-object1['secret number'] = 12341
+// object1.address = ['Helsinki']
+// object1['secret number'] = 12341
 
-console.log(object1.address)
-console.log(object1['secret number'])
+// console.log(object1.address)
+// console.log(object1['secret number'])
 
-//using object in array
+// //using object in array
 
-const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-console.log(parts[0].name)
+// const course = 'Half Stack application development'
+//   const parts = [
+//     {
+//       name: 'Fundamentals of React',
+//       exercises: 10
+//     },
+//     {
+//       name: 'Using props to pass data',
+//       exercises: 7
+//     },
+//     {
+//       name: 'State of a component',
+//       exercises: 14
+//     }
+//   ]
+// console.log(parts[0].name)
+
+//
+const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+course.parts.forEach(value => console.log(value.name))
+console.log(course.parts[0].name)
