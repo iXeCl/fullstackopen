e83447156be7521c06e91c1fcb0ec8dd2a138826
iXecl <dharmick95@gmail.com>
2024-10-01 12:05:30 +1000

    starting exercise 1.6

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,73 +1,39 @@
-const Header = (props) => {
-  const course = "Half Stack application development"
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-const Part = (props) => {
-  return (
-    <div>
-      <p> {props.part} {props.exercise} </p>
-    </div>
-  )
-}
-
-
-
-
-const Content = (props) => {
-  return (
-    <div> 
-        <Part part= {props.parts[0].name} exercises={props.parts[0].exercises}/>
-        <Part part= {props.parts[1].name} exercises={props.parts[1].exercises}/>
-        <Part part= {props.parts[2].name} exercises={props.parts[2].exercises}/>
-    </div>
-
-  )
-}
-
-
-const Total = (props) =>{
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-
-    </div>
-  )
-}
+import { useState } from 'react'
+const Display = ({counter}) => <div>{counter}</div>
+  
+const Button = ({onClick, text}) =>  <button onClick={onClick}> {text} </button>
+  
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = { 
+      left: clicks.left + 1, 
+      right: clicks.right 
+    }
+    setClicks(newClicks)
   }
-  console.log(course)
+
+  const handleRightClick = () => {
+    const newClicks = { 
+      left: clicks.left, 
+      right: clicks.right + 1 
+    }
+    setClicks(newClicks)
+  }
+
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/> 
-      <Total parts={course.parts}/>      
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
-    
   )
-  
 }
 
 export default App
-
--- learnjs.js
+++ learnjs.js
@@ -113,3 +113,24 @@ const course = {
   }
 course.parts.forEach(value => console.log(value.name))
 console.log(course.parts[0].name)
+
+//This statement
+
+const arto = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+  
+    greet: function() {
+      console.log('hello, my name is ' + this.name)
+    },
+  }
+  
+  arto.greet()  // "hello, my name is Arto Hellas" gets printed
+  arto.growOlder = function() {
+    this.age +=1
+  }
+  
+  console.log(arto.age)
+  arto.growOlder()
+  console.log(arto.age)
